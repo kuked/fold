f7120e0a599c79fd93cbf55c7507a149568de20c
kuked <aka.kudoken@gmail.com>
2016-08-17 21:51:19 +0900

    Add w flag to specify width

--- fold.go
+++ fold.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"unicode/utf8"
 )
 
+var width = flag.Int("w", 80, "")
+
 func main() {
-	files := os.Args[1:]
+	flag.Parse()
+	files := flag.Args()[0:]
 	if len(files) == 0 {
 		if err := view(os.Stdin); err != nil {
 			panic(err)
@@ -30,32 +34,32 @@ func main() {
 
 func view(f *os.File) error {
 	in := bufio.NewScanner(f)
-	in.Split(scan10Bytes)
+	in.Split(scanNRunes)
 	for in.Scan() {
 		fmt.Println(in.Text())
 	}
 	return in.Err()
 }
 
-func scan10Bytes(data []byte, atEOF bool) (advance int, token []byte, err error) {
+func scanNBytes(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if atEOF && len(data) == 0 {
 		return 0, nil, nil
 	}
-	if len(data) >= 10 {
-		return 10, data[0:10], nil
+	if len(data) >= *width {
+		return *width, data[0:*width], nil
 	}
 	return len(data), data, nil
 }
 
-func scan10Runes(data []byte, atEOF bool) (advance int, token []byte, err error) {
+func scanNRunes(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if atEOF && len(data) == 0 {
 		return 0, nil, nil
 	}
 
 	s := string(data)
-	if utf8.RuneCountInString(s) >= 10 {
+	if utf8.RuneCountInString(s) >= *width {
 		rs := []rune(s)
-		bs := []byte(string(rs[0:10]))
+		bs := []byte(string(rs[0:*width]))
 		return len(bs), bs, nil
 	}
 	return len(data), data, nil
