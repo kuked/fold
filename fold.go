e48202ce19a3e13a9d02707eea4dd6cc6fd9b903
kuked <aka.kudoken@gmail.com>
2016-08-21 11:04:55 +0900

    Add scanNColumns()

--- fold.go
+++ fold.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"unicode"
 	"unicode/utf8"
 )
 
@@ -35,7 +36,7 @@ func main() {
 
 func view(f *os.File) error {
 	in := bufio.NewScanner(f)
-	in.Split(scanNRunes)
+	in.Split(scanNColumns)
 	for in.Scan() {
 		fmt.Println(in.Text())
 	}
@@ -72,4 +73,66 @@ func scanNRunes(data []byte, atEOF bool) (advance int, token []byte, err error) 
 		return len(bs), bs, nil
 	}
 	return len(data), data, nil
+}
+
+var halfWidth = &unicode.RangeTable{
+	R16: []unicode.Range16{
+		{0xff61, 0xffbe, 1},
+		{0xffc2, 0xffc7, 1},
+		{0xffca, 0xffcf, 1},
+		{0xffd2, 0xffd7, 1},
+		{0xffda, 0xffdc, 1},
+		{0xffe9, 0xffee, 1},
+	},
+}
+
+func scanNColumns(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	// full ASCII characters.
+	s := string(data)
+	if len(data) == utf8.RuneCountInString(s) {
+		return scanNBytes(data, atEOF)
+	}
+
+	if *width >= utf8.RuneCountInString(s) {
+		return len(data), data, nil
+	}
+
+	// ASCII characters and non ASCII characters.
+	rs := ([]rune(s))[0:*width]
+	full := 0
+	half := 0
+	index := 0
+	for _, r := range rs {
+		bs := []byte(string(r))
+		if bs[0] < utf8.RuneSelf || unicode.In(r, halfWidth) {
+			half++
+			index++
+			if (full*2 + half) >= *width {
+				break
+			}
+		} else {
+			full++
+			index++
+			if *width == 1 {
+				break
+			}
+			if (full*2 + half) == *width {
+				break
+			}
+			if (full*2 + half) > *width {
+				index--
+				break
+			}
+		}
+	}
+
+	bs := []byte(string(rs[0:index]))
+	if i := bytes.IndexByte(bs, '\n'); i >= 0 {
+		return i + 1, bs[0:i], nil
+	}
+	return len(bs), bs, nil
 }
